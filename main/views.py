717902942076a803bd380a6ffb30c73979363e6e
s1rk0 <evolved.python@gmail.com>
2021-07-20 01:14:35 +0300

    Это просто переименованый файл в .html

--- main/views.py
+++ main/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 from django.shortcuts import redirect
+
+from .models import Article
 
 
 def index(request):
-    return(render(request, 'index.html'))
+    return (render(request, 'index.html'))
 
 
 def contacts(request):
-     return(render(request, 'contacts.html'))
+    return (render(request, 'contacts.html'))
+
+
+def see_more(request, article_id):
+    try:
+        a = Article.objects.get(id=article_id)
+    except:
+        raise Http404("Статьи нет, полудурок")
+    return (render(request, 'article.html'))
