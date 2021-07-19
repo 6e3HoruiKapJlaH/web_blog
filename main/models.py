0f04944842280df738b2148da41cda6b6bfe6d37
s1rk0 <evolved.python@gmail.com>
2021-07-20 01:12:05 +0300

    Пытался подрубить ссылку на статью

--- main/models.py
+++ main/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
+
 class Article(models.Model):
-    title = models.CharField('Article name', max_length = 200 )
-    text = models.TextField('Article text') 
-    img_url = models.URLField('picture png', max_length = 250)	
+    title = models.CharField('Article name', max_length=200)
+    text = models.TextField('Article text')
+    img_url = models.URLField('picture png', max_length=250)
 
--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('contacts', views.contacts), 
+    path('contacts', views.contacts),
+    path('<int:article_id>/', views.see_more, name = 'see_more'),
 ]
