fb71cb92a9cba7906a0f862f24da7011ad9a4219
s1rk0 <evolved.python@gmail.com>
2021-07-22 13:04:46 +0300

    Работающие кнопки Вперед и назад БД увеличена на 4 статьи

--- main/views.py
+++ main/views.py
@@ -7,7 +7,14 @@ from .models import Article
 
 def index(request):
     articles_list = Article.objects.order_by('-title')[:3]
-    return(render(request, 'index.html', {'articles_list' : articles_list}))
+    return (render(request, 'index.html', {'articles_list': articles_list}))
+
+
+def index_list(request, list_id):
+    len_list = Article.objects.count()/3
+    articles_list = Article.objects.order_by('-title')[3 * list_id:3 * (list_id + 1)]
+    return (render(request, 'index.html', {'articles_list': articles_list, 'list_id': list_id, 'back_id': list_id - 1,
+                                           'next_id': list_id + 1, 'len_list': len_list}))
 
 
 def contacts(request):
@@ -19,6 +26,4 @@ def see_more(request, article_id):
         a = Article.objects.get(id=article_id)
     except:
         raise Http404("Статьи нет, полудурок")
-    return (render(request, 'article.html', {'article' : a}))
-
-
+    return (render(request, 'article.html', {'article': a}))
