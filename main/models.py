abbe78e07e674f0f99d2f2e5f3d838a1158e9619
6e3HoruiKapJlaH <vedmacrl@gmail.com>
2021-07-23 22:36:07 +0300

    Пофиксить наллизание текста комментария на аватар

--- main/models.py
+++ main/models.py
@@ -7,3 +7,9 @@ class Article(models.Model):
     text = models.TextField('Article text') 
     img_url = models.URLField('picture png', max_length = 250)	
 
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    username = models.CharField('Author nickname', max_length = 50)
+    text = models.TextField('Comment text', max_length = 350)
+    #avatar = models.ImageField(upload_to='images')
--- main/views.py
+++ main/views.py
@@ -5,28 +5,47 @@ from django.shortcuts import redirect
 from .models import Article
 
 
-def clear_to_500_symb(article_list):
+def cut_to_500_symb(article_list):
     for a in article_list:
         a.text = a.text[:500] 
     return article_list    
 
+
 def index(request):
-    articles_list = clear_to_500_symb( Article.objects.order_by('title')[:3])
+    articles_list = cut_to_500_symb( Article.objects.order_by('title')[:3])
     
-    return (render(request, 'index.html', {'articles_list': articles_list}))
+    articles_dict = dict([
+        ('articles_list', articles_list)
+        ])
+
+    return (render(request, 'index.html', articles_dict))
 
 
 def index_list(request, list_id):
+
+    current_3_articles = Article.objects.order_by('title')[3 * (list_id - 1):3 * list_id]
+
     if list_id <= 0:
         raise Http404("Страницы нет, полудурок, уходи отсюда")
-    elif Article.objects.order_by('title')[3 * (list_id - 1):3 * list_id].count() == 0:
+    elif current_3_articles.count() == 0:
         raise Http404("Страницы нет, полудурок, уходи отсюда")
+
     else:
         len_list = Article.objects.count() / 3
-        articles_list = clear_to_500_symb(Article.objects.order_by('-title')[3 * (list_id - 1):3 * list_id])
-        return (
-            render(request, 'index.html', {'articles_list': articles_list, 'list_id': list_id, 'back_id': list_id - 1,
-                                           'next_id': list_id + 1, 'len_list': len_list}))
+
+        articles_list = current_3_articles
+        articles_list = cut_to_500_symb(articles_list)
+
+        articles_dict = dict([
+         ('articles_list', articles_list),
+         ('list_id', list_id), 
+         ('back_id', list_id - 1), 
+         ('next_id', list_id + 1), 
+         ('len_list', len_list)
+         ])
+
+        return (render(request, 'index.html', articles_dict))
+
 
 
 def contacts(request):
@@ -35,7 +54,11 @@ def contacts(request):
 
 def see_more(request, article_id):
     try:
-        a = Article.objects.get(id=article_id)
+        current_aticle = Article.objects.get(id=article_id)
     except:
         raise Http404("Статьи нет, полудурок")
-    return (render(request, 'article.html', {'article': a}))
+    
+    articles_dict = dict([
+         ('article', current_aticle)
+         ])
+    return (render(request, 'article.html', articles_dict))
